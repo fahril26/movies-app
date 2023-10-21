d0f551f6976550db1c6309f1a8a7c23cf0a74ffb
fahril26 <fahril.fhrl26@gmail.com>
2023-10-21 11:01:25 +0700

    edit MyNav

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/prop-types */
 import { Nav, NavLink } from "react-bootstrap";
 import "../style/MyNav.css";
+import { useNavigate } from "react-router-dom";
 
 const MyNav = ({ link, className, children, width }) => {
+  const navigate = useNavigate();
+
   const resetLocalStorage = () => {
     localStorage.setItem("currentPage", 1);
     localStorage.setItem("paginationNumbers", JSON.stringify([1, 2, 3, 4, 5]));
+
+    if (link) navigate(link);
   };
 
   return (
     <Nav className="my-nav" defaultActiveKey="/home">
       <Nav.Item style={width} className={className}>
         <NavLink
-          to={link ? link : null}
+          // to={link ? link : null}
           className="fw-semibold"
           onClick={resetLocalStorage}
         >
