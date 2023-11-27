8982c5ed901a44ceae9d8bc4f382d6446fcf24eb
fahril26 <fahril.fhrl26@gmail.com>
2023-11-27 15:22:04 +0700

    edit

--- src/components/LayoutMovieDetail.jsx
+++ src/components/LayoutMovieDetail.jsx
@@ -70,10 +70,10 @@ const LayoutMovieDetail = ({ type }) => {
         <Loading />
       ) : (
         <>
+          <CurrentPageContext>
+            <MyNavbar fixed={"top"} />
+          </CurrentPageContext>
           <div className="movie-detail">
-            <CurrentPageContext>
-              <MyNavbar fixed={"top"} />
-            </CurrentPageContext>
             <div className="container-fluid mt-5 px-0">
               <div className="content row  gap-md-3 gap-5 justify-content-center justify-content-md-start">
                 <div className="poster col-11 col-md-5 col-lg-4 col-xl-3  justify-content-center d-flex">
